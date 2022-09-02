9eb9b6d80236f7cba8254adc182eb29b54796b45
Lian-La-Fey <alamedrin@yandex.com>
2022-09-02 17:39:04 +0300

    Missing thing added.

--- src/Pages/Table/Lists.js
+++ src/Pages/Table/Lists.js
@@ -174,7 +174,7 @@ export const Lists = () => {
             <FaList className="me-4" />
             My Lists
           </h2>
-          <div className="d-flex align-items-center">
+          <div className="d-flex align-items-center position-relative">
             <Search
               placeholder="Search"
               value={search}
